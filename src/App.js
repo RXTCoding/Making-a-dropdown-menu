c2e99f577acc34d5419ff7029752e43b3ca8c522
RXTCoding <60244212+RXTCoding@users.noreply.github.com>
2021-05-06 09:09:09 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import "./styles.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({ showMenu: !this.state.showMenu });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>THIS IS A WEBSITE</h1>
+          <nav>
+            <ul className="nav-main">
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+            <ul className={`nav-drop ${this.state.showMenu ? "show" : ""}`}>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+            <button onClick={this.toggleMenu}>BURGER</button>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+}
